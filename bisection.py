276a6a6402d4495902f114c42821e6d847136230
rlBessa <61857348+rlBessa@users.noreply.github.com>
2022-04-19 21:28:49 -0300

    update

--- bisection.py
+++ bisection.py
@@ -1,11 +1,3 @@
-#---------------------------------------------------------
-# Given a continuous function f:[a,b]->R, with f(a)*f(b) <= 0,
-# this program,
-# using the bisection method,
-# returns a real root (or an approximate real root) for f,
-# with maximum absolute error given by tol
-#---------------------------------------------------------
-
 import math
 import numpy
 
